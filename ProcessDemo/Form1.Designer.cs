85e5887dd2db45e6a3b49bc56094d64b95eb6790
李中华 <247437853@qq.com>
2016-09-02 00:40:31 +0800

    https://github.com/chinalee1990/processdemo.git

--- ProcessDemo/Form1.Designer.cs
+++ ProcessDemo/Form1.Designer.cs
@@ -28,8 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.关闭ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -52,17 +57,49 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(189, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关闭ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(101, 26);
+            // 
+            // 关闭ToolStripMenuItem
+            // 
+            this.关闭ToolStripMenuItem.Name = "关闭ToolStripMenuItem";
+            this.关闭ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.关闭ToolStripMenuItem.Text = "关闭";
+            this.关闭ToolStripMenuItem.Click += new System.EventHandler(this.关闭ToolStripMenuItem_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(552, 415);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,6 +107,9 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 关闭ToolStripMenuItem;
     }
 }
 
--- ProcessDemo/Form1.cs
+++ ProcessDemo/Form1.cs
@@ -8,15 +8,29 @@ using System.Text;
 using System.Windows.Forms;
 using System.Diagnostics;
 
+
+using System.Runtime.InteropServices;  
+
+
 namespace ProcessDemo
 {
     
     public partial class Form1 : Form
     {
         Process m_process;
+        Form2 m_form2; 
         public Form1()
         {
             InitializeComponent();
+            int nScreenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int nScreenH = Screen.PrimaryScreen.WorkingArea.Height;
+            this.StartPosition = FormStartPosition.Manual;
+            this.Location = new Point(0, nScreenH / 2);
+            this.Width = nScreenWidth;
+            this.Height = nScreenH / 2;
+            this.KeyPreview = true;
+            //m_form2 = new Form2();
+            //m_form2.Show();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -55,5 +69,79 @@ namespace ProcessDemo
         {
             CloseProcess();
         }
+
+        [DllImport("user32.dll")]
+        public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+
+
+        [DllImport("user32.dll", EntryPoint = "ShowWindow", CharSet = CharSet.Auto)]
+        public static extern int ShowWindow(IntPtr hwnd, int nCmdShow);
+        //[DllImport("user32.dll")]
+        //public static extern void SwitchToThisWindow(IntPtr hWnd, bool fAltTab);
+        public const int SW_SHOWMAXIMIZED = 3;
+        //public const  int SW_MAXIMIZE = 3;
+        public const int SW_SHOWNOACTIVATE = 4;
+        //public const int SW_SHOW = 5;
+        private const int WM_CLOSE = 0x0010;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int y, int Width, int Height, int flags);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+
+            textBox1.Text = "激活";
+            //m_form2.Activate();
+            try
+            {
+                SwitchToThisWindow(m_process.MainWindowHandle, true);
+               // ShowWindow(m_process.MainWindowHandle, SW_SHOWNOACTIVATE);
+                //SetWindowPos(m_process.MainWindowHandle, -1, 0, 0, 0, 0, 1 | 2);
+                SetForegroundWindow(this.Handle);
+                //this.TopMost = true;
+                //Control control = Control.FromHandle(m_process.MainWindowHandle);
+                //Form form = (Form)control;
+                //form.Activate();
+                Debug.Print("激活窗口成功");
+            }
+            catch
+            {
+                Debug.Print("激活窗口失败：进程无效");
+            }
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            textBox1.Text = "未激活";
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            Debug.Print("Form1_Shown");
+
+        }
+
+        private void 关闭ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            this.contextMenuStrip1.Show(Cursor.Position);
+        }
+
+
     }
 }
